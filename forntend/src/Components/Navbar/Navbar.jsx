ee05a18de0edc2ed0235cdbb3c81700daf1f21d4
Diwash Tharu <diwash.tharu.ch@gmail.com>
2025-08-29 15:46:40 +0545

    hr tag

--- forntend/src/Components/Navbar/Navbar.jsx
+++ forntend/src/Components/Navbar/Navbar.jsx
@@ -11,7 +11,7 @@ export const Navbar = () => {
             <p>SHOPPING</p>
         </div>
         <ul className="nav-menu">
-            <li>Shop</li>
+            <li>Shop<hr/></li>
             <li>Men</li>
             <li>Women</li>
             <li>Kids</li>
