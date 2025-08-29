58ff9f095515b3449b15b271ebfc0a3c06e760b4
Diwash Tharu <diwash.tharu.ch@gmail.com>
2025-08-29 17:45:33 +0545

    setting the route in the page

--- forntend/src/App.js
+++ forntend/src/App.js
@@ -1,11 +1,27 @@
 // import logo from './logo.svg';
 import './App.css';
 import { Navbar } from './Components/Navbar/Navbar';
+import {BrowserRouter,Route,Routes,route} from "react-router-dom";
+import { ShopCategory } from './Pages/ShopCategory';
+import { Shop } from './Pages/Shop';
+// import product form './Pages/Product'
 
 function App() {
   return (
  <div>
+  <BrowserRouter>
   <Navbar/>
+  <Routes>
+    <Route path='/' element={<Shop/>}/>
+    <Route path='/mens' element={<ShopCategory category="men"/>}/>
+    <Route path='/womens' element={<ShopCategory category="women"/>}/>
+    <Route pathe='/product' element={<shopeCatrgory catrgory="product"/>}/>
+    <Route path='/kids' element={<shopeCategory category="kids"/>}/>
+    
+    <Route path=
+
+  </Routes>
+  </BrowserRouter>
  </div>
   );
 }
