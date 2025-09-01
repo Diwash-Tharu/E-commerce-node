7ca1e8fc56f378478da517b917b592849d8d6732
Diwash Tharu <diwash.tharu.ch@gmail.com>
2025-09-01 17:17:41 +0545

    importing the icon form the asserts files

--- forntend/src/Components/Hero/Hero.jsx
+++ forntend/src/Components/Hero/Hero.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import "./Hero.css"
 import hand_icon from "../Assets/hand_icon.png"
-import arrow_icon from "../Assets/arrow.png";
+import arrow_icon from "../Assets/arrow.png"
+import hero_image from "../Assets/hero_image.png"
+
 export const Hero = () => {
   return (
     <div className="hero">
@@ -17,13 +19,13 @@ export const Hero = () => {
             </div>
             <div className="hero-latest-btn">
                 <div>Latest Collection</div>
-                <img src={arrow_icon} alt="" />
+                <img src={arrow_icon} alt="arrow icon" />
 
             </div>    
         </div>
 
         <div className="hero-right">
-
+            <img src={hero_image} alt="hero"/>
         </div>
 
     </div>
