68d67cb2d6aef06e40d2c33a26dd8df2e39847f9
Diwash Tharu <diwash.tharu.ch@gmail.com>
2025-09-04 17:57:49 +0545

    adding the items

--- forntend/src/Components/Popular/Popular.jsx
+++ forntend/src/Components/Popular/Popular.jsx
@@ -3,19 +3,21 @@ import "./Popular.css"
 import data_product from '../Assets/data.js'
 import { Item } from '../Item/Item'
 
-export const Popular = () => {
+ const Popular = () => {
   return (
     <div className='popular'>
         <h1>POPULAR IN WOMEN</h1>
         <hr/>
 
         <div className="popluar-item">
-            {data_product.map((Item)=>{
-                return <Item key={i} id={Item.id} name={Item.name} image={Item.image} new_price={Item.new_price} old_price={Item.old_price}/>
-                
+            {data_product.map((item,i)=>{
+                return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price}/>
+
             })}
         </div>
 
     </div>
   )
 }
+
+export default Popular;
