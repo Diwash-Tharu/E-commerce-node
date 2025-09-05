c93515d2fb689cc3a43d8eb323aaf25cbac3a2f8
Diwash Tharu <diwash.tharu.ch@gmail.com>
2025-09-05 16:14:37 +0545

    importing the offers

---
+++ forntend/src/Components/offers/Offers.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import "./Offers.css"
+export const Offers = () => {
+  return (
+    <div className='offers'>
+        Offers</div>
+  )
+}
--- forntend/src/Pages/Shop.jsx
+++ forntend/src/Pages/Shop.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from "../Components/Hero/Hero"
 import Popular from "../Components/Popular/Popular.jsx"
+import  Offers  from '../Components/offers/Offers'
 
 export const Shop = () => {
   return (
     <div>
       <Hero/>
       <Popular/>
+      <Offers/>
     </div>
   )
 }
