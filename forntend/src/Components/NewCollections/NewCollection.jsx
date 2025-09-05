b150714d3a4c80763cae2e8e8be7b07049b1820a
Diwash Tharu <diwash.tharu.ch@gmail.com>
2025-09-05 16:49:26 +0545

    NEW COLLECTION FOLDERS

---
+++ forntend/src/Components/NewCollections/NewCollection.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './NewCollection.css'
+
+export const NewCollection = () => {
+  return (
+    <div className='newcollections'>
+        <h1>New Collection</h1>
+        <p>Discover our latest arrivals and exclusive offers.</p>
+        <button>Shop Now</button>
+    </div>
+  )
+}
