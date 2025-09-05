e2e19166384d9b8034c47bb0279a4b94672a8455
Diwash Tharu <diwash.tharu.ch@gmail.com>
2025-09-05 16:51:57 +0545

    importing the new collections

--- forntend/src/Components/NewCollections/NewCollection.jsx
+++ forntend/src/Components/NewCollections/NewCollection.jsx
@@ -1,12 +1,23 @@
 import React from 'react'
 import './NewCollection.css'
-
+import new_collection from '../Assets/new_collections'
 export const NewCollection = () => {
   return (
     <div className='newcollections'>
         <h1>New Collection</h1>
-        <p>Discover our latest arrivals and exclusive offers.</p>
-        <button>Shop Now</button>
+        <hr />
+
+        <div className="collections">
+            {new_collection.map((item, index) => (
+                <div className="collection-item" key={index}>
+                    <img src={item.image} alt={item.title} />
+                    <h2>{item.title}</h2>
+                    <p>{item.description}</p>
+                </div>
+            ))}
+        </div>
+        {/* <p>Discover our latest arrivals and exclusive offers.</p>
+        <button>Shop Now</button> */}
     </div>
   )
 }
