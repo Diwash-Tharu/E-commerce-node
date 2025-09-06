9573d5d0045dcd49ace5fa41bfe0807da5425fcf
Diwash Tharu <diwash.tharu.ch@gmail.com>
2025-09-06 17:02:33 +0545

    importing the new collections

--- forntend/src/Components/NewCollections/NewCollection.jsx
+++ forntend/src/Components/NewCollections/NewCollection.jsx
@@ -8,13 +8,17 @@ export const NewCollection = () => {
         <hr />
 
         <div className="collections">
-            {new_collection.map((item, index) => (
+            {/* {new_collection.map((item, index) => (
                 <div className="collection-item" key={index}>
                     <img src={item.image} alt={item.title} />
                     <h2>{item.title}</h2>
                     <p>{item.description}</p>
                 </div>
-            ))}
+            ))} */}
+
+            {new_collection.map((item,i)=>{
+                 return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price}/>
+            })}
         </div>
         {/* <p>Discover our latest arrivals and exclusive offers.</p>
         <button>Shop Now</button> */}
